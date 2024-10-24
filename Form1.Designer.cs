638db89c4660869bc7c5a76ce09f2e0f772f04c4
zeyne <zeyne@LAPTOP-27Q3J0O3>
2024-10-25 00:56:48 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace ŞirketUygulaması
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEposta = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtAdres = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGüncelle = new System.Windows.Forms.Button();
+            this.btnCıkıs = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(146)))), ((int)(((byte)(180)))));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 519);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(786, 168);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dataGridView1_CellFormatting);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(146)))), ((int)(((byte)(180)))));
+            this.label1.Location = new System.Drawing.Point(66, 162);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID";
+            // 
+            // txtID
+            // 
+            this.txtID.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(162)))), ((int)(((byte)(157)))), ((int)(((byte)(157)))));
+            this.txtID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtID.Location = new System.Drawing.Point(108, 156);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(176, 26);
+            this.txtID.TabIndex = 2;
+            // 
+            // txtAd
+            // 
+            this.txtAd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(162)))), ((int)(((byte)(157)))), ((int)(((byte)(157)))));
+            this.txtAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtAd.ForeColor = System.Drawing.Color.Black;
+            this.txtAd.Location = new System.Drawing.Point(108, 200);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(176, 26);
+            this.txtAd.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(146)))), ((int)(((byte)(180)))));
+            this.label2.Location = new System.Drawing.Point(60, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "AD";
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(162)))), ((int)(((byte)(157)))), ((int)(((byte)(157)))));
+            this.txtSoyad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtSoyad.Location = new System.Drawing.Point(108, 244);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(176, 26);
+            this.txtSoyad.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(146)))), ((int)(((byte)(180)))));
+            this.label3.Location = new System.Drawing.Point(26, 250);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "SOYAD";
+            // 
+            // txtEposta
+            // 
+            this.txtEposta.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(162)))), ((int)(((byte)(157)))), ((int)(((byte)(157)))));
+            this.txtEposta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtEposta.Location = new System.Drawing.Point(108, 288);
+            this.txtEposta.Name = "txtEposta";
+            this.txtEposta.Size = new System.Drawing.Size(176, 26);
+            this.txtEposta.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(146)))), ((int)(((byte)(180)))));
+            this.label4.Location = new System.Drawing.Point(14, 294);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "E-POSTA";
+            // 
+            // txtTel
+            // 
+            this.txtTel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(162)))), ((int)(((byte)(157)))), ((int)(((byte)(157)))));
+            this.txtTel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtTel.Location = new System.Drawing.Point(108, 332);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(176, 26);
+            this.txtTel.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(146)))), ((int)(((byte)(180)))));
+            this.label5.Location = new System.Drawing.Point(10, 338);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "TELEFON";
+            // 
+            // txtAdres
+            // 
+            this.txtAdres.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(162)))), ((int)(((byte)(157)))), ((int)(((byte)(157)))));
+            this.txtAdres.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtAdres.Location = new System.Drawing.Point(108, 376);
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.Size = new System.Drawing.Size(176, 26);
+            this.txtAdres.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(146)))), ((int)(((byte)(180)))));
+            this.label6.Location = new System.Drawing.Point(26, 382);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 18);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "ADRES";
+            // 
+            // btnListele
+            // 
+            this.btnListele.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(146)))), ((int)(((byte)(180)))));
+            this.btnListele.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnListele.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(58)))), ((int)(((byte)(102)))));
+            this.btnListele.Location = new System.Drawing.Point(476, 141);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(127, 53);
+            this.btnListele.TabIndex = 13;
+            this.btnListele.Text = "LİSTELE";
+            this.btnListele.UseVisualStyleBackColor = false;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(146)))), ((int)(((byte)(180)))));
+            this.btnKaydet.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.btnKaydet.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(58)))), ((int)(((byte)(102)))));
+            this.btnKaydet.Location = new System.Drawing.Point(476, 215);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(127, 53);
+            this.btnKaydet.TabIndex = 14;
+            this.btnKaydet.Text = "KAYDET";
+            this.btnKaydet.UseVisualStyleBackColor = false;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(146)))), ((int)(((byte)(180)))));
+            this.btnSil.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.btnSil.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(58)))), ((int)(((byte)(102)))));
+            this.btnSil.Location = new System.Drawing.Point(476, 289);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(127, 53);
+            this.btnSil.TabIndex = 15;
+            this.btnSil.Text = "SİL";
+            this.btnSil.UseVisualStyleBackColor = false;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGüncelle
+            // 
+            this.btnGüncelle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(146)))), ((int)(((byte)(180)))));
+            this.btnGüncelle.Font = new System.Drawing.Font("Calibri", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.btnGüncelle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(58)))), ((int)(((byte)(102)))));
+            this.btnGüncelle.Location = new System.Drawing.Point(477, 363);
+            this.btnGüncelle.Name = "btnGüncelle";
+            this.btnGüncelle.Size = new System.Drawing.Size(127, 53);
+            this.btnGüncelle.TabIndex = 16;
+            this.btnGüncelle.Text = "GÜNCELLE";
+            this.btnGüncelle.UseVisualStyleBackColor = false;
+            this.btnGüncelle.Click += new System.EventHandler(this.btnGüncelle_Click);
+            // 
+            // btnCıkıs
+            // 
+            this.btnCıkıs.BackColor = System.Drawing.Color.Transparent;
+            this.btnCıkıs.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnCıkıs.BackgroundImage")));
+            this.btnCıkıs.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnCıkıs.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCıkıs.Location = new System.Drawing.Point(781, 2);
+            this.btnCıkıs.Name = "btnCıkıs";
+            this.btnCıkıs.Size = new System.Drawing.Size(28, 29);
+            this.btnCıkıs.TabIndex = 17;
+            this.btnCıkıs.UseVisualStyleBackColor = false;
+            this.btnCıkıs.Click += new System.EventHandler(this.btnCıkıs_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(810, 709);
+            this.Controls.Add(this.btnCıkıs);
+            this.Controls.Add(this.btnGüncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.txtAdres);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtTel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtEposta);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEposta;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtAdres;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnListele;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGüncelle;
+        private System.Windows.Forms.Button btnCıkıs;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ŞirketUygulaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Entities db = new Entities();  // veritabanı bağlantısı için bir Entities nesnesi oluşturma
+        private void RemoveCustomer(int id) 
+        {
+            
+            var musteri = db.tblMusteriler.Find(id); //veri tabanında belirtilen id ye sahip müşteri kaydını bulur
+            if (musteri != null)
+            {
+                musteri.durum = false; // müşteri kayıt durumunu false yaparak tamamen silmez 
+                db.SaveChanges(); //değişiklikleri veri tabanına kaydeder
+                MessageBox.Show("Kayıt başarıyla kaldırıldı.");
+            }
+            else
+            {
+                MessageBox.Show("Kayıt bulunamadı.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            listele(); // Kaydı listeyi güncelle
+        }
+        public void listele() 
+        {
+            dataGridView1.DataSource = db.tblMusteriler.ToList(); //tüm kayıtlar
+            txtID.Text = "";
+            txtAd.Text = "";
+            txtSoyad.Text = "";
+            txtEposta.Text = "";
+            txtTel.Text = "";
+            txtAdres.Text = "";
+
+
+
+        }
+        
+        private void Form1_Load(object sender, EventArgs e) //form açıldığında çalışan metot
+        {
+            listele();
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            { //tüm alanlar dolu mu ?
+                if (txtAd.Text == "" || txtSoyad.Text == "" || txtEposta.Text == "" || txtTel.Text == "" || txtAdres.Text == "")
+
+                    MessageBox.Show("Lütfen tüm alanları giriniz", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                else 
+                {
+                    tblMusteriler x = new tblMusteriler();
+                    x.ad = txtAd.Text;
+                    x.soyad = txtSoyad.Text;
+                    x.eposta = txtEposta.Text;
+                    x.telefon = txtTel.Text;
+                    x.adres = txtAdres.Text;
+                    x.durum = true;
+
+                    db.tblMusteriler.Add(x); //yeni kişiyi ekler
+                    db.SaveChanges(); // veritabanına kaydeder
+                    MessageBox.Show("Kayıt Başarılı");
+                }
+                listele();
+
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e) // bir hücreye tıklandığında
+        { //tüm alanları doldurur
+            txtID.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            txtAd.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            txtSoyad.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            txtEposta.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            txtTel.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            txtAdres.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        { //ID boş mu ?
+            if (txtID.Text == "")
+                MessageBox.Show("Lütfen silmek istediğiniz kişiyi seçiniz", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else
+            {
+                int id = int.Parse(txtID.Text); //id alır int türüne dönüştürür
+                var x = db.tblMusteriler.Find(id); //belirtilen id ye sahip müşteriyi bulur
+                x.durum = false; //durum pasif
+                db.SaveChanges(); //veritabanına kaydeder
+                MessageBox.Show("Müşteri silindi");
+            }
+            listele();
+        }
+
+        private void btnGüncelle_Click(object sender, EventArgs e)
+        { //dolu mu?
+            if (txtAd.Text == "" || txtSoyad.Text == "" || txtEposta.Text == "" || txtTel.Text == "" || txtAdres.Text == "")
+                MessageBox.Show("Lütfen güncellenecek kişiyi seçiniz", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else
+            {
+                int id = int.Parse(txtID.Text); //id alır int türüne dönüştürür
+                var x = db.tblMusteriler.Find(id); //belirtilen id ye sahip müşteriyi bulur
+                x.ad = txtAd.Text; // alanı günceller
+                x.soyad = txtSoyad.Text;
+                x.eposta = txtEposta.Text;
+                x.telefon = txtTel.Text;
+                x.adres = txtAdres.Text;
+                db.SaveChanges();
+                MessageBox.Show("Güncelleme yapıldı");
+            }
+
+            listele();
+        }
+
+        private void btnCıkıs_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); 
+        }
+
+        private void dataGridView1_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+       
+            if (dataGridView1.Columns[e.ColumnIndex].Name == "durum") //durum sütununu bul 
+            {
+                bool durum = Convert.ToBoolean(e.Value); //değeri bool a çevir
+                if (!durum) 
+                {
+                    dataGridView1.Rows[e.RowIndex].DefaultCellStyle.BackColor = Color.LightGray; // pasif müşterileri gri 
+                }
+                else 
+                {
+                    dataGridView1.Rows[e.RowIndex].DefaultCellStyle.BackColor = Color.LightSkyBlue; // aktif müşteri rengi mavi
+                }
+            }
+        }
+    }
+}
